0dc816cfe11af305f0cdc0e9ab83ebe8073150c3
Ivan Holub <ivanholub@gmail.com>
2016-04-19 00:07:25 +0200

    webcam

--- web/js/main.js
+++ web/js/main.js
@@ -285,6 +285,38 @@ var main = (function (executor) {
         });
     }
 
+    var imageNr = 0; // Serial number of current image
+    var finished = new Array(); // References to img objects which have finished downloading
+    var paused = false;
+
+    function createImageLayer() {
+        var img = new Image();
+        img.style.position = "absolute";
+        img.style.zIndex = -1;
+        img.onload = imageOnload;
+        img.onclick = imageOnclick;
+        img.src = "http://192.168.2.50:8080/?action=snapshot&n=" + (++imageNr);
+        img.width = 300;
+        var webcam = document.getElementById("webcam");
+        webcam.insertBefore(img, webcam.firstChild);
+    }
+
+// Two layers are always present (except at the very beginning), to avoid flicker
+    function imageOnload() {
+        this.style.zIndex = imageNr; // Image finished, bring to front!
+        while (1 < finished.length) {
+            var del = finished.shift(); // Delete old image(s) from document
+            del.parentNode.removeChild(del);
+        }
+        finished.push(this);
+        if (!paused) createImageLayer();
+    }
+
+    function imageOnclick() { // Clicking on the image will pause the stream
+        paused = !paused;
+        if (!paused) createImageLayer();
+    }
+
     function docReady() {
         console.log("ready!");
         executor.init(onMsg);
@@ -295,6 +327,7 @@ var main = (function (executor) {
         $("#pingBtn").click(function () {
             sendPing();
         });
+        createImageLayer();
     }
 
     var init = function () {
